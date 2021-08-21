7fc0e96dec3bccbcc296749cdcd11d8795f77c93
Christopher <[nwachukwuchristopher92@gmail.com]>
2021-08-21 12:29:04 +0100

    Search button implementation

--- pages/index.js
+++ pages/index.js
@@ -34,15 +34,22 @@ export default function Home() {
       </header>
 
       {/**Body */}
-      <form>
+      <form className="flex flex-col items-center mt-44 flex-grow">
         <Image src="https://www.google.co.uk/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
         height={100}
         width={300}
         />
-        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center">
+        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl
+        lg:max-w-2xl">
           <SearchIcon className="h-5 mr-3 text-gray-500"/>
           <input type="text" className="flex-grow focus:outline-none"/>
           <MicrophoneIcon className="h-5"/>
+        </div>
+
+        <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
+          <button className="btn">Google Search</button>
+          
+          <button className="btn">I'm Feeling Lucky</button>
         </div>
       </form>
 
