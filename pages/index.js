0fd8ee4a1ca5ec325f347a5268ede18d1fadb3d1
Christopher <[nwachukwuchristopher92@gmail.com]>
2021-08-27 14:43:08 +0100

    My Header, HeaderOptions

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
 import Head from 'next/head'
 import Avatar from '../components/Avatar';
-import { MicrophoneIcon, SearchIcon, ViewGridIcon } from '@heroicons/react/solid';
-import Image from 'next/image'
+import { MicrophoneIcon, ViewGridIcon } from '@heroicons/react/solid';
+import { SearchIcon } from '@heroicons/react/outline';
+import Image from 'next/image';
 import Footer from '../components/Footer';
+import { useRef } from 'react';
+import { useRouter } from "next/router"; 
 
 
 export default function Home() {
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+
+  const search = (e) => {
+    e.preventDefault();
+    const term = searchInputRef.current.value;
+
+    if (!term) return;
+
+    router.push(`/search?term=${term}`);
+  };
+
   return (
     <div className="flex flex-col justify-center h-screen">
       <Head>
@@ -43,14 +58,13 @@ export default function Home() {
         <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl
         lg:max-w-2xl">
           <SearchIcon className="h-5 mr-3 text-gray-500"/>
-          <input type="text" className="flex-grow focus:outline-none"/>
+          <input ref={searchInputRef} type="text" className="flex-grow focus:outline-none"/>
           <MicrophoneIcon className="h-5"/>
         </div>
 
         <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
-          <button className="btn">Google Search</button>
-          
-          <button className="btn">I'm Feeling Lucky</button>
+          <button onClick={search} className="btn">Google Search</button>
+          <button onClick={search} className="btn">I'm Feeling Lucky</button>
         </div>
       </form>
 
